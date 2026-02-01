a9eef7dfff5e3559689dbea79bb2fd22da846d96
johnny4young <asesordeprogramacion@gmail.com>
2026-01-31 19:07:23 -0500

    feat: handled router app to the startup electron

--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { useOfflineSync, useOfflineCapability } from './useOfflineSync';
 export { useTenantSettings } from './useTenantSettings';
 export { useTableExport } from './useTableExport';
+export { useElectron } from './useElectron';
 
 // API hooks
 export * from './api';
